801ae242a769201896cc93e02528b50203f05fd3
Anupama Garani <agarani95@gmail.com>
2019-05-27 14:41:32 -0400

    Update 242. Valid Anagram-bruteForce.py

--- 242. Valid Anagram-bruteForce.py
+++ 242. Valid Anagram-bruteForce.py
@@ -6,24 +6,21 @@ class Solution:
         t = ''.join(sorted(t))
         
         pos = 0
-        i = 0
         anagramVal = False
         lens = len(s)
         lent = len(t)
         
         # If length of s and t are not same then they are not anagrams
         if lens == lent:
-            
             # While length of s is less than t
             while pos < lens and not anagramVal:
                 
                 # If the characters comparison is successful increment
-                if s[i]==t[i]:
-                    i=i+1
+                if s[pos]==t[pos]:
+                    pos=pos+1
                 else:
                     anagramVal = False
                     return anagramVal
-                pos = pos+1
             return True
         else:
             return False
