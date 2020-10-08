d06c60cf03213144cb6ae7347b1e01986fd65127
Anupama Garani <anupamagarani95@gmail.com>
2020-10-08 10:36:50 -0400

    Update 3. Longest Substring Without Repeating Characters.py

--- Medium/3. Longest Substring Without Repeating Characters.py
+++ Medium/3. Longest Substring Without Repeating Characters.py
@@ -1,29 +1,37 @@
+
+
 class Solution:
+    
+    def check_duplicate(self,str_a):
+        unique_set = set()
+        if len(str_a)==1:
+            return 1
+        else:
+            for i in str_a:
+                if i in unique_set:
+                    return 0
+                unique_set.add(i)
+        return len(unique_set)
+        
     def lengthOfLongestSubstring(self, s: str) -> int:
-        passes=0
+        
         if len(s)==0:
             return 0
         if len(s)==1:
             return 1
         else:
-            result_list = []
-            index_dict={}
-            traverse = 0
-            res_set = set()
-            for i in range(0,len(s)):
-                if s[i] not in res_set:
-                    res_set.add(s[i])
-                    result_list.append(s[:i+1])  
-                else:
-                    passes+=1
-                    index_dict[passes]=i
-                    # Compare the previous substrings with the current one 
-                    if passes>1:
-                        result_list.append(s[index_dict[passes-1]:i])
-                    else:
-                        result_list.append(s[:i])  
-            
-            print(result_list)
-            res = [len(x) for x in result_list]
-            print(res)
-            return max(res)
+            s0=s[0]
+            if all(c == s0 for c in s[1:]) == True:
+                return 1
+            sub_dict={}
+            list_len=[]
+            for i in range(0,len(s)-1):
+                for j in range(i+1,len(s)):
+                    #check if it has duplicate characters and get the length add to dict
+                    # get the highest length in dict
+                    res = self.check_duplicate(s[i:j+1])
+                    list_len.append(res)
+            if len(list_len)==0:
+                return 0
+            else:
+                return max(list_len)
