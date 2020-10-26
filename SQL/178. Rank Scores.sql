c38fabf826f17d721e10e447920520e4a81b9d3e
Anupama Garani <anupamagarani95@gmail.com>
2020-10-26 11:27:06 -0400

    Update and rename 178. Rank Scores to 178. Rank Scores.sql

---
+++ SQL/178. Rank Scores.sql
@@ -0,0 +1,42 @@
+/*
+
+#https://www.sqlservertutorial.net/sql-server-window-functions/sql-server-dense_rank-function/
+
+Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+For example, given the above Scores table, your query should generate the following report (order by highest score):
+
++-------+---------+
+| score | Rank    |
++-------+---------+
+| 4.00  | 1       |
+| 4.00  | 1       |
+| 3.85  | 2       |
+| 3.65  | 3       |
+| 3.65  | 3       |
+| 3.50  | 4       |
++-------+---------+
+*/
+
+
+select Score as 'score',
+DENSE_RANK() OVER (ORDER BY Score DESC) 'Rank'
+From Scores
+
+/*
+Dense Rank
+1) Window function
+2) Consecutive ranking with no holes
+
+*/
+
